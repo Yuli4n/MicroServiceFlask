547c50ed551326cb2dec67e603c37080df6b083b
Yuli4n <j.contreras11@uniandes.edu.co>
2024-05-24 13:49:31 -0500

    Testing

--- ProductManager/models.py
+++ ProductManager/models.py
@@ -3,13 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Customer(db.Model):
+    __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    products = db.relationship('FinancialProduct', backref='customer', lazy=True)
+    financial_products = db.relationship('FinancialProduct', backref='customer', lazy=True)
 
 class FinancialProduct(db.Model):
+    __tablename__ = 'financial_products'
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100), nullable=False)
-    balance = db.Column(db.Float, nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    balance = db.Column(db.Float, nullable=False)  # Asegúrate de que el nombre del campo coincide
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
