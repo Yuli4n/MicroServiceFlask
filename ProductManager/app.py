f52ee081e71e11e9bb1e47bf53009fed0a74cd5d
Yuli4n <j.contreras11@uniandes.edu.co>
2024-05-24 12:58:36 -0500

    front2

--- ProductManager/app.py
+++ ProductManager/app.py
@@ -1,25 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 from config import Config
 from models import db, Customer, FinancialProduct
 
 app = Flask(__name__)
 app.config.from_object(Config)
+Bootstrap(app)
 
 db.init_app(app)
 
 @app.route('/')
 def index():
-    return "Welcome to the Financial Products Service!"
+    return render_template('index.html')
 
-@app.route('/customers/<int:id>', methods=['GET'])
-def get_customer(id):
-    customer = Customer.query.get_or_404(id)
-    products = FinancialProduct.query.filter_by(customer_id=id).all()
-    return {
-        'name': customer.name,
-        'email': customer.email,
-        'products': [{'product_name': p.product_name, 'balance': p.balance} for p in products]
-    }
+@app.route('/search', methods=['POST'])
+def search():
+    email = request.form.get('email')
+    customer = Customer.query.filter_by(email=email).first()
+    products = []
+    if customer:
+        products = FinancialProduct.query.filter_by(customer_id=customer.id).all()
+    return render_template('index.html', products=products, email=email)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
